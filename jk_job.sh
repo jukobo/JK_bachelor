9cb20b976a61c095478dbe6c1bcc9b61e85e0af7
jukobo <s214704@student.dtu.dk>
2024-03-23 08:57:08 +0100

    --

--- jk_job.sh
+++ jk_job.sh
@@ -2,9 +2,9 @@
 
 #SBATCH --job-name=jk_model
 #SBATCH --output=jk_model_%J.out
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=6
 #SBATCH --time=4-12:00:00
-#SBATCH --mem=8gb
+#SBATCH --mem=12gb
 #SBATCH --gres=gpu:Turing:1
 #SBATCH --mail-user=s214704@dtu.dk
 #SBATCH --mail-type=BEGIN,END,FAIL
@@ -22,7 +22,7 @@ fi
 
 source ~/.bashrc
 module load CUDA/11.4
-source .bscenv/bin/activate
+source bscenv/bin/activate
 python VertebraeSegmentation/Verse/Training.py --no-mps 
 
 echo "Done: $(date +%F-%R:%S)"

eabac072c03bda0fad19d09d3994e3c906c3f5ab
jukobo <s214704@student.dtu.dk>
2024-03-24 17:54:46 +0100

    ---

--- jk_job.sh
+++ jk_job.sh
@@ -22,7 +22,8 @@ fi
 
 source ~/JK_bachelor/.bashrc
 module load CUDA/11.4
-source bsc-env/bin/activate
+# source bsc-env/bin/activate
+conda bsc-env activate
 python VertebraeSegmentation/Verse/Training.py --no-mps 
 
 echo "Done: $(date +%F-%R:%S)"

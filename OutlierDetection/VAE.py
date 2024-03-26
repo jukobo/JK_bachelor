26c97c8f676d1413bae06ea8cadb316576f463a8
KatjaNorsker <katja@norsker.com>
2024-03-26 14:04:41 +0100

    Updated dataloader

--- OutlierDetection/VAE.py
+++ OutlierDetection/VAE.py
@@ -3,6 +3,7 @@ import torch.nn.functional as F
 import torch.nn as nn
 from torch.utils.data import Dataset
 import os
+import numpy as np
 
 
 
@@ -18,6 +19,37 @@ class LoadData(Dataset):
     def __len__(self):
         return len(self.images)
     
+    def __getitem__(self, index):
+        img_path = os.path.join(self.img_dir,self.images[index])
+        msk_path = os.path.join(self.msk_dir,self.images[index].replace('img.npy','msk.npy'))
+        distfield_path = os.path.join(self.heatmap_dir,self.images[index].replace('img.npy','distfield.npy'))
+
+        #INPUTS
+        #Load image
+        img = np.load(img_path)
+        #Load distfield
+        distfield = np.load(distfield_path)
+
+        #Load mask
+        msk = np.load(msk_path)
+        msk = msk.astype(np.float64)
+    
+        #Input tensor
+        inputs = np.stack((img, distfield), axis=3)
+        #Reshape
+        inputs = np.moveaxis(inputs, -1, 0)
+        inputs = inputs.astype(np.float64)
+                
+        #Add one dimension to mask to set #channels = 1
+        msk = np.expand_dims(msk, axis=0)
+
+        #Convert to tensor
+        inputs = torch.from_numpy(inputs)
+        msk = torch.from_numpy(msk)
+
+        subject = self.images[index].split("_")[0]
+
+        return inputs, msk, subject
 
 
 

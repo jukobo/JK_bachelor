e22445ba336e83227ec6f5f7ea77a9823e2deec0
jukobo <s214704@student.dtu.dk>
2024-03-30 15:33:44 +0100

    ..

--- jk_job.sh
+++ jk_job.sh
@@ -15,12 +15,12 @@ echo "Node: $(hostname)"
 echo "Start: $(date +%F-%R:%S)"
 echo -e "Working dir: $(pwd)\n"
 
-SCRATCH=/scratch/$USER
-if [[ ! -d $SCRATCH ]]; then
-  mkdir $SCRATCH
-fi
+# SCRATCH=/scratch/$USER
+# if [[ ! -d $SCRATCH ]]; then
+#   mkdir $SCRATCH
+# fi
 
-source ~/JK_bachelor
+source ~/JK_bachelor/.bashrc
 module load CUDA/11.4
 source bsc-env/bin/activate
 python VertebraeSegmentation/Verse/Training.py --no-mps 

1076943200ff4dc080fe5503e0502f2c998b8e01
jukobo <s214704@student.dtu.dk>
2024-03-30 15:30:06 +0100

    ---

--- jk_job.sh
+++ jk_job.sh
@@ -2,9 +2,9 @@
 
 #SBATCH --job-name=jk_model
 #SBATCH --output=jk_model_%J.out
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-task=4
 #SBATCH --time=7-00:00:00
-#SBATCH --mem=16gb
+#SBATCH --mem=32gb
 #SBATCH --gres=gpu:Turing:1
 #SBATCH --mail-user=s214704@dtu.dk
 #SBATCH --mail-type=BEGIN,END,FAIL
@@ -20,7 +20,7 @@ if [[ ! -d $SCRATCH ]]; then
   mkdir $SCRATCH
 fi
 
-source ~/JK_bachelor/.bashrc
+source ~/JK_bachelor
 module load CUDA/11.4
 source bsc-env/bin/activate
 python VertebraeSegmentation/Verse/Training.py --no-mps 

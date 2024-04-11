f925e5dfa9324ee48a73af9d08c1aa944c5ce4c5
jukobo <s214704@student.dtu.dk>
2024-04-11 12:14:15 +0200

    --

--- OutlierDetection/our_training_test2.py
+++ OutlierDetection/our_training_test2.py
@@ -8,8 +8,8 @@ from VAE import *
 
 #Define paramters
 parameters_dict = {
-    'epochs': 50,
-    'learning_rate': 1e-4,
+    'epochs': 100,
+    'learning_rate': 1e-5,
     'batch_size': 1, #Noget galt når batch size ændres til mere end 1
     'weight_decay': 5e-4,
 }

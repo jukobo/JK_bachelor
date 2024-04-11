4643d096d88b57329a7d0ccf304a29b8f6e16a46
jukobo <s214704@student.dtu.dk>
2024-04-11 13:38:50 +0200

    ..

--- OutlierDetection/J_test.py
+++ OutlierDetection/J_test.py
@@ -2,8 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from VAE import *
 
-no = 2400
-file = f'C:/Users/julie/OneDrive/Skrivebord/Bachelor/JK_bachelor/OutlierDetection/rec_data/reconstruction{no}.npy'
+no = 750
+# file = f'C:/Users/julie/OneDrive/Skrivebord/Bachelor/JK_bachelor/OutlierDetection/rec_data/reconstruction{no}.npy'
+file = f'/scratch/s214704/Data/Checkpoints/VertebraeSegmentation/rec_img/rec_img_step{no}_batchsize1_lr1e-05_wd0.0005.npy'
 x = np.load(file)
 print(x[:,64,:].shape)
 

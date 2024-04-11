e2487ac355987bdb1e70b1b5fd6a0e16fad84e18
jkboel <juliekofodboel@gmail.com>
2024-04-11 13:37:46 +0200

    ..

--- OutlierDetection/J_test.py
+++ OutlierDetection/J_test.py
@@ -2,11 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 from VAE import *
 
-no = 2400
-file = f'C:/Users/julie/OneDrive/Skrivebord/Bachelor/JK_bachelor/OutlierDetection/rec_data/reconstruction{no}.npy'
+no = 900
+file = f'C:/Users/julie/OneDrive/Skrivebord/Bachelor/JK_bachelor/OutlierDetection/rec_data/rec_img_step900_batchsize1_lr1e-05_wd0.0005.npy'
 x = np.load(file)
-print(x[:,64,:].shape)
+print(x.shape)
+# print(x[:,64,:].shape)
 
-plt.imshow(x[:,64,:], cmap='gray')
-plt.show()
+# plt.imshow(x[:,64,:], cmap='gray')
+# plt.show()
 

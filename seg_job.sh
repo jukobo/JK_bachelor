587179cb102f12139b2c05a7e56b84148a8884c5
Knorsker <katja@norsker.com>
2024-04-18 13:53:31 +0200

    updated

--- seg_job.sh
+++ seg_job.sh
@@ -4,8 +4,9 @@
 #SBATCH --output=jk_spine_model_%J.out
 #SBATCH --cpus-per-task=2
 #SBATCH --time=1-05:00:00
-#SBATCH --mem=32gb
-#SBATCH --gres=gpu:Turing:1
+#SBATCH --mem=8gb
+#SBATCH --gres=gpu:2
+#SBATCH --nodelist=comp-gpu02
 #SBATCH --mail-user=s214725@dtu.dk
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --export=ALL
@@ -22,7 +23,6 @@ fi
 
 source ~/JK_bachelor/.bashrc
 module load CUDA/11.4
-source bsc-env/bin/activate
 # python VertebraeSegmentation/Verse/Predict_mask_titans.py --no-mps 
 python SpineLocalisation/Verse/Preprocessing_NOPADDING.py --no-mps 
 

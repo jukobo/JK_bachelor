47cb6a52abe1921b4756986a2f1bb4c99eb6ba2c
Knorsker <katja@norsker.com>
2024-04-19 14:31:50 +0200

    Updated

--- seg_job.sh
+++ seg_job.sh
@@ -3,8 +3,8 @@
 #SBATCH --job-name=jk_stage2_spine_model
 #SBATCH --output=jk_stage2_spine_model_%J.out
 #SBATCH --cpus-per-task=2
-#SBATCH --time=1-05:00:00
-#SBATCH --mem=8gb
+#SBATCH --time=7-00:00:00
+#SBATCH --mem=32gb
 #SBATCH --gres=gpu:2
 #SBATCH --mail-user=s214725@dtu.dk
 #SBATCH --mail-type=BEGIN,END,FAIL
@@ -23,8 +23,7 @@ fi
 
 source ~/JK_bachelor/.bashrc
 module load CUDA/11.4
-# python VertebraeLocalisation_singleclass/Verse/Data_preprocessing/Preprocessing.py --no-mps 
-python VertebraeLocalisation_singleclass/Verse/Data_preprocessing/heatmap_generation.py --no-mps 
+python VertebraeLocalisation_singleclass/Verse/Training.py --no-mps 
 
 
 

ad100523bc7c591308953ab7e992b414462a9eb5
Knorsker <katja@norsker.com>
2024-04-19 14:20:17 +0200

    Updated

--- VertebraeLocalisation_singleclass/Verse/Training.py
+++ VertebraeLocalisation_singleclass/Verse/Training.py
@@ -35,20 +35,16 @@ parameters_dict = {
 
 #For everything
 #gpu-cluster
-img_dir_training = 'Data/Verse20/VertebraeLocalisation/Verse20_training_prep/img' #'/Users/andreasaspe/Documents/Data/Verse20/Verse20_training_prep/img' #'/scratch/s174197/data/Verse20/Verse20_training_prep/img' #'/Users/andreasaspe/Documents/Data/Verse20_training_prep/img' #r'C:\Users\PC\Documents\Andreas_s174197\Preprocessed_data\img'
-heatmap_dir_training = 'Data/Verse20/VertebraeLocalisation/Verse20_training_prep/heatmaps'
-img_dir_validation ='Data/Verse20/VertebraeLocalisation/Verse20_validation_prep/img'
-heatmap_dir_validation = 'Data/Verse20/VertebraeLocalisation/Verse20_validation_prep/heatmaps'
+img_dir_training = '/scratch/s214725/Data/Verse20/Verse20_training_prep/img' #'/Users/andreasaspe/Documents/Data/Verse20_training_prep/img' #r'C:\Users\PC\Documents\Andreas_s174197\Preprocessed_data\img'
+heatmap_dir_training = '/scratch/s214725/Data/Verse20/VertebraeLocalisation/Verse20_training_prep/heatmaps'
+img_dir_validation ='/scratch/s214725/Data/Verse20/VertebraeLocalisation/Verse20_validation_prep/img'
+heatmap_dir_validation = '/scratch/s214725/Data/Verse20/VertebraeLocalisation/Verse20_validation_prep/heatmaps'
 
-checkpoint_dir = 'Data/Checkpoints/VertebraeLocalisation/NO_DATAAUG' #'/Users/andreasaspe/Library/Mobile Documents/com~apple~CloudDocs/DTU/12.semester/Thesis/My_code/My_networks/Spine_Localisation/Checkpoints'
+checkpoint_dir = '/scratch/s214725/Data/Checkpoints/VertebraeLocalisation/NO_DATAAUG' #'/Users/andreasaspe/Library/Mobile Documents/com~apple~CloudDocs/DTU/12.semester/Thesis/My_code/My_networks/Spine_Localisation/Checkpoints'
 
 run_name = 'FIXED_DATAAUG_both' #REMEBER TO CHANGE ABOVE FOLDER ALSO, In general change this to save to a different checkpoint 'Third_try_No_dropout_newinitialisation' #No underscore after this
 description = 'Nu har jeg fikset data augmentation. Stupid me. Har tilføjet self. LIGEMEGET! Fejlen var i if-statement i both! Kører med both!'
-#mac
-# img_dir_training = '/Users/andreasaspe/Documents/Data/Verse20/VertebraeLocalisation/Verse20_training_prep/img' #'/Users/andreasaspe/Documents/Data/Verse20/Verse20_training_prep/img' #'/scratch/s174197/data/Verse20/Verse20_training_prep/img' #'/Users/andreasaspe/Documents/Data/Verse20_training_prep/img' #r'C:\Users\PC\Documents\Andreas_s174197\Preprocessed_data\img'
-# heatmap_dir_training = '/Users/andreasaspe/Documents/Data/Verse20/VertebraeLocalisation/Verse20_training_heatmaps'
-# img_dir_validation = '/Users/andreasaspe/Documents/Data/Verse20/VertebraeLocalisation/Verse20_validation_prep/img' #'/Users/andreasaspe/Documents/Data/Verse20/Verse20_training_prep/img' #'/scratch/s174197/data/Verse20/Verse20_training_prep/img' #'/Users/andreasaspe/Documents/Data/Verse20_training_prep/img' #r'C:\Users\PC\Documents\Andreas_s174197\Preprocessed_data\img'
-# heatmap_dir_validation = '/Users/andreasaspe/Documents/Data/Verse20/VertebraeLocalisation/Verse20_validation_heatmaps'
+
 
 #Create checkpoint parent folder if it does not exist
 os.makedirs(checkpoint_dir, exist_ok=True)
@@ -181,8 +177,14 @@ for epoch in range(num_epochs):
             'transform': transform
 
         }
+
+           #Save checkpoint every 100 epoch
+    if epoch%100 == 0:
         torch.save(checkpoint, os.path.join(checkpoint_dir,str(run_name)+'_epoch'+str(epoch)+'_batchsize'+str(batch_size)+'_lr'+str(lr)+'_wd'+str(wd)+'.pth'))
-        # torch.save(checkpoint, os.path.join(checkpoint_dir,str(run_name)+'_batchsize'+str(batch_size)+'_lr'+str(lr)+'_wd'+str(wd)+'.pth'))
+
+
+torch.save(checkpoint, os.path.join(checkpoint_dir,str(run_name)+'_epoch'+str(epoch)+'_batchsize'+str(batch_size)+'_lr'+str(lr)+'_wd'+str(wd)+'.pth'))
+
 
 
 

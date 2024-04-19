58f149894286805989da4cc0efa4d9e88ff285bc
Knorsker <katja@norsker.com>
2024-04-19 11:21:26 +0200

    Updated

--- seg_job.sh
+++ seg_job.sh
@@ -24,7 +24,8 @@ fi
 
 source ~/JK_bachelor/.bashrc
 module load CUDA/11.4
-python SpineLocalisation/Verse/Preprocessing_NOPADDING.py --no-mps 
+# python SpineLocalisation/Verse/Training.py --no-mps 
+python SpineLocalisation/Verse/Preprocessing.py --no-mps 
 
 
 

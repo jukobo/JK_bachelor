aa1b2eadc18cc1fb7c7b32fe8fa9a7a21a44ebe6
jukobo <s214704@student.dtu.dk>
2024-05-02 11:28:39 +0200

    ---

--- OutlierDetection/our_training_conv.py
+++ OutlierDetection/our_training_conv.py
@@ -266,7 +266,7 @@ def train2D_conv(model, optimizer, epochs, device):
                         #-- Loss function
                         squared_diff = (inputs_reconstructed - inputs) ** 2
                         print(squared_diff.shape)
-                        v_loss = torch.mean(squared_diff, dim=0)
+                        v_loss = torch.mean(squared_diff, dim=1)
                         print(v_loss.shape)
                         exit()
 

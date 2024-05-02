3477fca444f910a5350c62eb1631748efee1ae06
jukobo <s214704@student.dtu.dk>
2024-05-02 11:47:43 +0200

    ---

--- OutlierDetection/our_training_conv.py
+++ OutlierDetection/our_training_conv.py
@@ -302,12 +302,12 @@ def train2D_conv(model, optimizer, epochs, device):
             print(f'Epoch {epoch+1}, Average loss: {overall_loss/(n_2-n_1+1)}')    
 
         ## Save model
-        if epoch == 0:
-            torch.save(model.state_dict(), f'/scratch/{study_no_save}/Data/model_conv_{epoch}.pth')
-            print('Model saved')
-        elif epoch == epochs-1:
-            torch.save(model.state_dict(), f'/scratch/{study_no_save}/Data/model_conv_{epoch}.pth')
-            print('Model saved')
+        # if epoch == 0:
+        #     torch.save(model.state_dict(), f'/scratch/{study_no_save}/Data/model_conv_{epoch}.pth')
+        #     print('Model saved')
+        # elif epoch == epochs-1:
+        #     torch.save(model.state_dict(), f'/scratch/{study_no_save}/Data/model_conv_{epoch}.pth')
+        #     print('Model saved')
 
     # np.save('OutlierDetection/o_loss3.npy', o_loss)
     # np.save('OutlierDetection/val_loss3.npy', val_loss)
